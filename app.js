8f165aef97bc0ca99207248dec2d82de1639522b
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-07-14 12:47:25 +0400

    javascript and
    javascript and

---
+++ app.js
@@ -0,0 +1,54 @@
+var myName = "shmagi";
+const myAge = 25;
+let mylastName = "narsavidze";
+
+mylastName = "narsavidze-narsavidze";
+
+console.log("age-", myAge, "name-", myName, "lastname-", mylastName);
+console.log(`age- ${myAge}, name- ${myName}, lastname- ${mylastName}.`);
+
+const num1 = 30;
+const num2 = 39.9;
+const add = num1 + num2;
+const add1 = num1 * num2;
+const add2 = num1 / num2;
+const add3 = num1 % num2;
+
+console.log(typeof num1, typeof num2);
+console.log(add);
+console.log(add1);
+console.log(add2);
+console.log(add3);
+
+const text1 = "burti 'da' ";
+const text2 = 'kari "da"';
+const text3 = `moedani 
+
+text new line`;
+
+console.log(text1, text2, text3);
+
+const itIsPassword = false;
+console.log(typeof itIsPassword);
+
+const num3 = null;
+console.log(typeof num3);
+
+let myName1 = "shmagi";
+let myHobby = '"football"';
+let yearOfBirth = 1997;
+let currentYear = 2023;
+let myAge1 = currentYear - yearOfBirth;
+
+console.log(
+  "my name is- ",
+  myName,
+  "I am- ",
+  myAge1,
+  "yers old and, my hobbi is- ",
+  myHobby
+);
+
+console.log(
+  `my name is- ${myName1}, I am- ${myAge1}  yers old and, my hobbi is- ${myHobby} `
+);
